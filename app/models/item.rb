eebb10a092944ffe30ae4d596b8b0b57b9c25489
JennyVong <jennyvong502@gmail.com>
2022-05-16 23:07:13 -0400

    full CRUD operations complete

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  validates :sku, presence: true
+  validates :name, presence: true
+  validates :desc, presence: true, length: { minimum: 10 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
